23b2413eed756f238842ecd3d3b8608d8ebeb03b
Evely <evelyn.vitoria2606@gmail.com>
2026-05-13 21:58:32 -0300

    corsnovo

--- src/main/java/com/e_library/config/CorsConfig.java
+++ src/main/java/com/e_library/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry){
                 registry.addMapping("/**")
-                        .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500", "https://e-library-ashy-eta.vercel.app")
+                        .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500", "https://e-library-ashy-eta.vercel.app", "http://127.0.0.1:3000")
                         .allowedMethods("*");
             }
  
